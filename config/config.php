10f962691be2326d459b75892945b518c7f8e23d
imtiajinfo <33751817+imtiajinfo@users.noreply.github.com>
2020-03-07 22:15:22 +0600

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php 
+	//define('ROOT_URL', 'http://localhost/skilljobs/simpleblog/');
+	define('DB_HOST', 'localhost');
+	define('DB_USER', 'root');
+	define('DB_PASS', '');
+	define('DB_NAME', 'simple-blog');
+	
+
+ ?>
